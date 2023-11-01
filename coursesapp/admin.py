d69b2764ad6bb17b33cf4a791e991b97ce5ffd82
jshuckbot <gorka_ivan@mail.ru>
2023-11-01 18:27:46 +0700

    added model for contents

--- coursesapp/admin.py
+++ coursesapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from coursesapp import models as coursesapp_models
+
+
+@admin.register(coursesapp_models.Subject)
+class SusjectAdmin(admin.ModelAdmin):
+    list_display = ["title", "slug"]
+    prepopulated_fields = {"slug": ("title",)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = coursesapp_models.Module
+
+
+@admin.register(coursesapp_models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ["title", "subject", "created"]
+    list_filter = ["created", "subject"]
+    search_fields = ["title", "overview"]
+    prepopulated_fields = {"slug": ("title",)}
+    inlines = [ModuleInline]
