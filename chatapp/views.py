3b9d002152c8f5697394386d09667d5e2d1751fb
jshuckbot <gorka_ivan@mail.ru>
2023-12-04 13:34:50 +0700

    added channels and daphne

---
+++ chatapp/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404, render
+
+
+@login_required
+def course_chat_room(request, course_id):
+    try:
+        course = request.user.courses_joined.get(id=course_id)
+    except:
+        return HttpResponseForbidden()
+    return render(request, "chatapp/room.html", {"course": course})
