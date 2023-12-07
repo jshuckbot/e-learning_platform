6a39651294283bdddc12c624d56f3c95aaf0243f
jshuckbot <gorka_ivan@mail.ru>
2023-12-07 23:16:53 +0700

    added cashe redis and db postgres

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,5 @@
+import os
+
 from config.settings.base import *
 
 DEBUG = False
@@ -6,6 +8,19 @@ ADMINS = [
     ("Ivan G", "admin@mail.ru"),
 ]
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["educaproject.com", "www.educaproject.com"]
 
-DATABASES = {"default": {}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
+
+REDIS_URL = "redis://cache:6379"
+CACHES["default"]["LOCATION"] = REDIS_URL
+CHANNEL_LAYERS["default"]["CONFIG"]["hosts"] = [REDIS_URL]
