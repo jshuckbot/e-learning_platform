c2adf2faf73a271a7000ea6f806d2501b940673a
jshuckbot <gorka_ivan@mail.ru>
2023-12-08 12:42:15 +0700

    setting midlleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "coursesapp.middleware.subdomain_course_middleware",
 ]
 
 ROOT_URLCONF = "config.urls"
