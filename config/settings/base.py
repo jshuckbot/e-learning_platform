d3c0006af4170baba5383e53cf266ec004a13434
Ivan <93859862+jshuckbot@users.noreply.github.com>
2023-12-08 12:45:21 +0700

    Deployment (#5)
    * implemented deploy

--- config/settings/base.py
+++ config/settings/base.py
@@ -3,17 +3,14 @@ from pathlib import Path
 from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$b4j@3sil01#7p(kk8-41p0j3*tmv#jonn)u=pcxd=7@xv*k5#"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = "$b4j@3sil01#7p(kk8-41p0j3*tmv#jonn)u=pcxd=7@xv*k5#"
 
 ALLOWED_HOSTS = [
     "0.0.0.0",
@@ -53,6 +50,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "coursesapp.middleware.subdomain_course_middleware",
 ]
 
 ROOT_URLCONF = "config.urls"
@@ -78,14 +76,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
---
+++ config/settings/prod.py
@@ -0,0 +1,30 @@
+import os
+
+from config.settings.base import *
+
+DEBUG = False
+
+ADMINS = [
+    ("Ivan G", "admin@mail.ru"),
+]
+
+ALLOWED_HOSTS = [".educaproject.com"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
+
+REDIS_URL = "redis://cache:6379"
+CACHES["default"]["LOCATION"] = REDIS_URL
+CHANNEL_LAYERS["default"]["CONFIG"]["hosts"] = [REDIS_URL]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
